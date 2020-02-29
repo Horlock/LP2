f4725e68372aa509ab8149f68451ae7052097185
Horlock <vinicius.fernando.souza@gmail.com>
2020-02-29 16:55:05 -0300

    corrigindo as pastas

---
+++ atividade1/CalculaVolume/CalculaVolume.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CalculaVolume {
+    partial class titulo {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.raioTxt = new System.Windows.Forms.TextBox();
+            this.alturaTxt = new System.Windows.Forms.TextBox();
+            this.volumeTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(153, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Altura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Raio";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // raioTxt
+            // 
+            this.raioTxt.Location = new System.Drawing.Point(55, 28);
+            this.raioTxt.Name = "raioTxt";
+            this.raioTxt.Size = new System.Drawing.Size(92, 20);
+            this.raioTxt.TabIndex = 3;
+            this.raioTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // alturaTxt
+            // 
+            this.alturaTxt.Location = new System.Drawing.Point(204, 29);
+            this.alturaTxt.Name = "alturaTxt";
+            this.alturaTxt.Size = new System.Drawing.Size(92, 20);
+            this.alturaTxt.TabIndex = 4;
+            this.alturaTxt.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // volumeTxt
+            // 
+            this.volumeTxt.Location = new System.Drawing.Point(105, 109);
+            this.volumeTxt.Name = "volumeTxt";
+            this.volumeTxt.ReadOnly = true;
+            this.volumeTxt.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.volumeTxt.Size = new System.Drawing.Size(92, 20);
+            this.volumeTxt.TabIndex = 6;
+            this.volumeTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.volumeTxt.TextChanged += new System.EventHandler(this.volumeTxt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Volume";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(23, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(113, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(203, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 46);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // titulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 216);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.volumeTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.alturaTxt);
+            this.Controls.Add(this.raioTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "titulo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular Volume";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox raioTxt;
+        private System.Windows.Forms.TextBox alturaTxt;
+        private System.Windows.Forms.TextBox volumeTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
