4be0c0ba1498215eda7bda19badc3686078257ab
Horlock <vinicius.fernando.souza@gmail.com>
2020-03-17 20:43:22 -0300

    ativade 2 finalizada

--- atividade1/CalculaVolume/CalculaVolume.Designer.cs
+++ atividade1/CalculaVolume/CalculaVolume.Designer.cs
@@ -38,9 +38,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(153, 29);
+            this.label2.Location = new System.Drawing.Point(205, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Size = new System.Drawing.Size(53, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Altura";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -49,36 +50,40 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 30);
+            this.label3.Location = new System.Drawing.Point(17, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.Size = new System.Drawing.Size(43, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Raio";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // raioTxt
             // 
-            this.raioTxt.Location = new System.Drawing.Point(55, 28);
+            this.raioTxt.Location = new System.Drawing.Point(74, 22);
+            this.raioTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.raioTxt.Name = "raioTxt";
-            this.raioTxt.Size = new System.Drawing.Size(92, 20);
+            this.raioTxt.Size = new System.Drawing.Size(121, 22);
             this.raioTxt.TabIndex = 3;
             this.raioTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // alturaTxt
             // 
-            this.alturaTxt.Location = new System.Drawing.Point(204, 29);
+            this.alturaTxt.Location = new System.Drawing.Point(273, 24);
+            this.alturaTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.alturaTxt.Name = "alturaTxt";
-            this.alturaTxt.Size = new System.Drawing.Size(92, 20);
+            this.alturaTxt.Size = new System.Drawing.Size(121, 22);
             this.alturaTxt.TabIndex = 4;
             this.alturaTxt.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // volumeTxt
             // 
-            this.volumeTxt.Location = new System.Drawing.Point(105, 109);
+            this.volumeTxt.Location = new System.Drawing.Point(141, 122);
+            this.volumeTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.volumeTxt.Name = "volumeTxt";
             this.volumeTxt.ReadOnly = true;
             this.volumeTxt.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.volumeTxt.Size = new System.Drawing.Size(92, 20);
+            this.volumeTxt.Size = new System.Drawing.Size(121, 22);
             this.volumeTxt.TabIndex = 6;
             this.volumeTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.volumeTxt.TextChanged += new System.EventHandler(this.volumeTxt_TextChanged);
@@ -87,9 +92,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 89);
+            this.label1.Location = new System.Drawing.Point(166, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.Size = new System.Drawing.Size(65, 20);
             this.label1.TabIndex = 5;
             this.label1.Text = "Volume";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -97,9 +103,10 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Location = new System.Drawing.Point(23, 158);
+            this.button1.Location = new System.Drawing.Point(32, 182);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 46);
+            this.button1.Size = new System.Drawing.Size(112, 57);
             this.button1.TabIndex = 7;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = false;
@@ -108,9 +115,10 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Yellow;
-            this.button2.Location = new System.Drawing.Point(113, 158);
+            this.button2.Location = new System.Drawing.Point(152, 182);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 46);
+            this.button2.Size = new System.Drawing.Size(112, 57);
             this.button2.TabIndex = 8;
             this.button2.Text = "Limpar";
             this.button2.UseVisualStyleBackColor = false;
@@ -119,9 +127,10 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button3.Location = new System.Drawing.Point(203, 158);
+            this.button3.Location = new System.Drawing.Point(272, 182);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(84, 46);
+            this.button3.Size = new System.Drawing.Size(112, 57);
             this.button3.TabIndex = 9;
             this.button3.Text = "Sair";
             this.button3.UseVisualStyleBackColor = false;
@@ -129,9 +138,9 @@
             // 
             // titulo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 216);
+            this.ClientSize = new System.Drawing.Size(417, 266);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -141,6 +150,7 @@
             this.Controls.Add(this.raioTxt);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "titulo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calcular Volume";
---
+++ atividade2/atividade2/CalculaPesoIdeal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade2
+{
+    public partial class CalculaPesoIdeal : Form
+    {
+        public CalculaPesoIdeal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtPeso.Clear();
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double peso, altura;
+
+            if (double.TryParse(txtPeso.Text, out peso) &&
+                double.TryParse(txtAltura.Text, out altura))
+            {
+                double pesoIdeal;
+                if (radioF.Checked)
+                { // feminino
+                    pesoIdeal = (62.1 * altura) - 44.7;
+                }
+                else
+                { // masculino
+                    pesoIdeal = (72.7 * altura) - 58;
+                }
+                string pesoIdealStr = pesoIdeal.ToString("N2");
+                if(pesoIdeal < peso)
+                {
+                    MessageBox.Show("Peso acima do ideal: " + pesoIdealStr + "kg." +
+                                    "\nSeu peso: " + peso + "kg.");
+                }else if(pesoIdeal > peso)
+                {
+                    MessageBox.Show("Peso abaixo do ideal: " + pesoIdealStr + "kg." +
+                                    "\nSeu peso: " + peso + "kg.");
+                }
+                else
+                {
+                    MessageBox.Show("Peso dentro do recomendado: " + pesoIdealStr + "kg.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dados inválidos!");
+            }
+        }
+    }
+}
